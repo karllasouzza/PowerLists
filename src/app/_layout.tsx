efe58c39eb8f26a4fd55b26b8449f2e97fb3772d
Karlla Souzza <karlla.souzza7@gmail.com>
2025-12-02 21:48:17 -0300

    refactor: Remove `SafeAreaView` from the root layout.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -7,7 +7,6 @@ import { useEffect } from 'react';
 import ThemeProvider from '@/context/themes/use-themes';
 import { useAuthStore } from '@/stores/auth';
 import '@/css/global.css';
-import { SafeAreaView } from 'react-native-safe-area-context';
 import LoadingPage from './loading';
 
 export default function RootLayout() {
@@ -27,16 +26,14 @@ export default function RootLayout() {
   }
   return (
     <ThemeProvider name="default">
-      <SafeAreaView className="flex-1 bg-background">
-        <Stack>
-          <Stack.Screen name="index" options={{ headerShown: false }} />
-          <Stack.Screen name="login" options={{ headerShown: false }} />
-          {/* <Stack.Screen name="create-account" options={{ headerShown: false }} />
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="login" options={{ headerShown: false }} />
+        {/* <Stack.Screen name="create-account" options={{ headerShown: false }} />
         <Stack.Screen name="reset-password" options={{ headerShown: false }} /> */}
-        </Stack>
-        <PortalHost />
-        <Toaster />
-      </SafeAreaView>
+      </Stack>
+      <PortalHost />
+      <Toaster />
     </ThemeProvider>
   );
 }
