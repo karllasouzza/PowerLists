83b564a138d4ad3a92b220e5151f269893e26fdf
Karlla Souzza <karlla.souzza7@gmail.com>
2026-03-31 22:43:11 -0300

    feat(radio-group): add RadioGroup and RadioGroupItem components
    - Introduced RadioGroup and RadioGroupItem components using @rn-primitives/radio-group
    - Implemented forwardRef for better integration
    - Styled components for consistent UI

---
+++ src/components/ui/radio-group.tsx
@@ -0,0 +1,37 @@
+import { cn } from '@/lib/utils';
+import * as RadioGroupPrimitive from '@rn-primitives/radio-group';
+import React from 'react';
+import { Platform } from 'react-native';
+
+const RadioGroup = React.forwardRef<
+  RadioGroupPrimitive.RootRef,
+  React.ComponentProps<typeof RadioGroupPrimitive.Root>
+>(function RadioGroup({ className, ...props }, ref) {
+  return <RadioGroupPrimitive.Root ref={ref} className={cn('gap-3', className)} {...props} />;
+});
+
+const RadioGroupItem = React.forwardRef<
+  RadioGroupPrimitive.ItemRef,
+  React.ComponentProps<typeof RadioGroupPrimitive.Item>
+>(function RadioGroupItem({ className, ...props }, ref) {
+  return (
+    <RadioGroupPrimitive.Item
+      ref={ref}
+      className={cn(
+        'border-input dark:bg-input/30 aspect-square size-4 shrink-0 items-center justify-center rounded-full border shadow-sm shadow-black/5',
+        Platform.select({
+          web: 'focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive outline-none transition-all focus-visible:ring-[3px] disabled:cursor-not-allowed',
+        }),
+        props.disabled && 'opacity-50',
+        className,
+      )}
+      {...props}>
+      <RadioGroupPrimitive.Indicator className="bg-primary size-2 rounded-full" />
+    </RadioGroupPrimitive.Item>
+  );
+});
+
+RadioGroup.displayName = 'RadioGroup';
+RadioGroupItem.displayName = 'RadioGroupItem';
+
+export { RadioGroup, RadioGroupItem };
