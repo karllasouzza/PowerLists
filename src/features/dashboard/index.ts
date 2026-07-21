e701c2b8fe9585d88d2d24f4727a5c2e9ad48c7d
Karlla Souzza <karlla.souzza7@gmail.com>
2026-07-21 18:28:06 -0300

    chore: remove old dashboard feature (replaced by summary)

--- src/features/dashboard/index.ts
+++
@@ -1,6 +0,0 @@
-export { default } from './page';
-export * from './types';
-export * from './utils';
-export * from './components';
-export * from './hooks/use-dashboard-page';
-export * from './hooks/use-item-comparison';
